6a06011b9f06e328a62022e5c088599bd7b01b6e
garogross <garikgevorgyan.rap@mail.ru>
2024-06-12 15:40:25 +0400

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function DashboardCostomersPage() {
+    return (
+        <div>DashboardCostomersPage</div>
+    );
+}
+
+export default DashboardCostomersPage;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function DashboardInvoicesPage() {
+    return (
+        <div>DashboardInvoicesPage</div>
+    );
+}
+
+export default DashboardInvoicesPage;
